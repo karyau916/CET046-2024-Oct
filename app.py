9016f32f1a53d9dfe6cb29a08cdcaca4929c0cb6
karyau916 <leongkaryau@gmail.com>
2024-10-19 06:16:13 +0000

    1

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
-           
+
 @app.route("/prediction_DBS",methods=["GET","POST"])
 def prediction_DBS():
-    return(render_template("prediction_DBS.html"))
+    q = float(request.form.get("q"))
+    return(render_template("prediction_DBS.html",r=90.2+(-50.6*q)))
 
 if __name__ == "__main__":
     app.run()
